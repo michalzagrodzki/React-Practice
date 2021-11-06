c87abdf33380d22546cb906d1b135ad299ba4a05
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-11-06 21:59:34 +0100

    feat: Update layout of Home view

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,16 +1,40 @@
 import React from "react";
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import { Grid, Container, Typography, Card, CardContent, CardActions, Button } from '@mui/material';
 
 export default function Home() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Welcome to React exercises
-        </Typography>
-      </Box>
-    </Container>
+    <Grid container spacing={0}>
+      <Grid item md={6}>
+        <Container>
+          <Typography variant="h4" component="h1" gutterBottom>
+            React exercises
+          </Typography>
+          <Typography variant="h6" component="h4" gutterBottom>
+            Dive into react exerciess. See different approaches, use of libraries and methods.
+          </Typography>
+        </Container>
+      </Grid>
+      <Grid item md={6}>
+        <Container>
+          <Grid container spacing={2}>
+            <Grid item md={4}>
+              <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    Chat mockup
+                  </Typography>
+                  <Typography variant="body2">
+                    Structure for mockup chat with communication between components
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grid>
+    </Grid>
   );
 }
