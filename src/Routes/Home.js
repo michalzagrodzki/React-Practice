a0668fec5cb716eea2d113f9c56d78f8dadd6f5c
michal zagrodzki <michalzagrodzki@gmail.com>
2022-01-29 22:14:58 +0100

    feat: update layout for Home view

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Card from "./../components/card";
+import Empty from "./../components/empty"
+import list from "./../Data/index"
 
 export default function Home() {
+  const [exerciseLinks, setExerciseLinks] = useState([]);
+  useEffect(() => {
+    setExerciseLinks(list)
+  }, []);
+  
   return (
     <div>
       <h1>Welcome to react exercises</h1>
+      {
+        exerciseLinks.length > 0 &&
+        exerciseLinks.map((link, index) => {
+        return (
+          <Card
+            key={index}
+            title={link.title} 
+            content={link.description} 
+            link={link.link} 
+          />
+        )
+      })}
+      {
+        exerciseLinks.length === 0 &&
+        <Empty message={"No links available"} />
+      }
     </div>
   );
 }
