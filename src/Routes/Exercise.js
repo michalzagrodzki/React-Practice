0ab5f5e2568becc2ed111cbda8eb6587143a4183
michal zagrodzki <michalzagrodzki@gmail.com>
2022-01-31 08:35:57 +0100

    refactor: change structure for fetching data into exercise component

--- src/Routes/Exercise.js
+++ src/Routes/Exercise.js
@@ -1,23 +1,33 @@
-import React, {lazy} from "react";
+import React, {lazy, useState, useEffect} from "react";
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 export default function Exercise() {
-  const codeString = `function Exercise() {
-    return (
-      <div>
-        <h1>This is react example</h1>
-      </div>
-    );
-  }`
   const ComponentName = "usestate";
-  const ExerciseComponent = lazy(() => import(`./../Exercises/${ComponentName}`));
-
+  const [exerciseTitle, setExerciseTitle] = useState("This is react exercise");
+  const [exerciseCodeString, setExerciseCodeString] = useState("");
+  const [ExerciseComponent, setExerciseComponent] = useState(lazy(() => import(`./../Exercises/${ComponentName}`)));
+  
+  useEffect(() => {
+    setExerciseTitle("This is react exercise");
+    setExerciseCodeString(`function Exercise() {
+      return (
+        <div>
+          <h1>This is react example</h1>
+        </div>
+      );
+    }`);
+    setExerciseComponent(lazy(() => import(`./../Exercises/${ComponentName}`)));
+  }, []);
+  
   return (
     <div>
-      <h1>This is react exercise</h1>
-      <SyntaxHighlighter language="javascript" style={atomOneDark}>
-        {codeString}
+      <h1>{ exerciseTitle }</h1>
+      <SyntaxHighlighter 
+        language="javascript" 
+        style={atomOneDark}
+      >
+        { exerciseCodeString }
       </SyntaxHighlighter>
       <ExerciseComponent />
     </div>
