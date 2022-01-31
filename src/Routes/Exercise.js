d1697f10c94c3738df97eaaa04861ed12043c125
michal zagrodzki <michalzagrodzki@gmail.com>
2022-01-31 08:06:12 +0100

    feat: add support for syntax highlight and jsx parse

--- src/Routes/Exercise.js
+++ src/Routes/Exercise.js
@@ -1,9 +1,31 @@
 import React from "react";
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import JsxParser from 'react-jsx-parser'
 
 export default function Exercise() {
+
+  const codeString = `function Exercise() {
+    return (
+      <div>
+        <h1>This is react example</h1>
+      </div>
+    );
+  }`
+
+  const codeBlock = `<div>
+    <h1>This is react example</h1>
+  </div>`
+
   return (
     <div>
       <h1>This is react exercise</h1>
+      <SyntaxHighlighter language="javascript" style={atomOneDark}>
+        {codeString}
+      </SyntaxHighlighter>
+      <JsxParser
+        jsx={codeBlock}
+      />
     </div>
   );
 }
