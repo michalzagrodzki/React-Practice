b7b8a8456a3a2b3bf21b52887a600da384e615df
michal zagrodzki <michalzagrodzki@gmail.com>
2022-02-10 22:07:46 +0100

    feat: add layout for exercise view

--- src/Routes/Exercise.js
+++ src/Routes/Exercise.js
@@ -3,6 +3,7 @@ import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
 import js from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript';
 import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import { useParams } from 'react-router-dom';
+import { Grid, Box, Container, Typography, Card, CardContent} from '@mui/material';
 import ExercisesData from './../Data/exercises';
 
 SyntaxHighlighter.registerLanguage('javascript', js);
@@ -23,16 +24,84 @@ export default function Exercise() {
   }, []);
   
   return (
-    <div>
-      <h1>{ exerciseTitle }</h1>
-      <p>{ exerciseCaption }</p>
-      <SyntaxHighlighter
-        language="javascript" 
-        style={atomOneDark}
+    <Box 
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent:'center',
+        alignItems: 'center',
+        height: "100vh",
+        backgroundColor: 'primary.dark',
+      }}
+    >
+      <Container sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent:'center',
+        alignItems: 'center',
+      }}>
+        <Typography 
+          variant="h4" 
+          component="h1" 
+          gutterBottom sx={{color: "white"}}
+        >
+          { exerciseTitle }
+        </Typography>
+        <Typography 
+          variant="h6" 
+          component="h4" 
+          gutterBottom sx={{color: "white"}}
+        >
+          { exerciseCaption }
+        </Typography>
+      </Container>
+      <Grid 
+        container 
+        spacing={1}
+        alignItems="center"
+        maxWidth={1600}
       >
-        { exerciseCodeString }
-      </SyntaxHighlighter>
-      <ExerciseComponent />
-    </div>
+        <Grid item md={6}>
+          <Card>
+            <CardContent 
+              sx={{
+                padding: "0px !important"
+              }}
+            >
+              <SyntaxHighlighter
+                language="javascript" 
+                style={atomOneDark}
+              >
+                { exerciseCodeString }
+              </SyntaxHighlighter>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid 
+          item 
+          md={6}
+          sx={{
+            height: "100%"
+          }}
+        >
+          <Card 
+            sx={{
+              height: "100%"
+            }}
+          >
+            <CardContent 
+              sx={{
+                padding: "0px !important",
+                color: "white",
+                backgroundColor: 'primary.light',
+                height: "100%"
+              }}
+            >
+              <ExerciseComponent />
+            </CardContent>
+          </Card>
+        </Grid>
+    </Grid>
+    </Box>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,10 +5,10 @@ export default function Header({ title, caption }) {
   return (
     <Grid item md={4}>
       <Container>
-        <Typography variant="h4" component="h1" gutterBottom>
+        <Typography variant="h4" component="h1" gutterBottom sx={{color: "white"}}>
           {title}
         </Typography>
-        <Typography variant="h6" component="h4" gutterBottom>
+        <Typography variant="h6" component="h4" gutterBottom sx={{color: "white"}}>
           {caption}
         </Typography>
       </Container>
