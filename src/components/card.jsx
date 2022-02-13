2b961489c5761633ce2f1cdb686f3bdb4f8b1b0f
michal zagrodzki <michalzagrodzki@gmail.com>
2022-02-13 22:39:40 +0100

    feat: update styles in home view

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Typography, Card, CardContent, CardActions, Button } from '@mui/material';
+import { lightBlue, grey } from '@mui/material/colors';
 
 export default function ExerciseCard({ title, content, link }) {
   let navigate = useNavigate();
@@ -12,19 +13,27 @@ export default function ExerciseCard({ title, content, link }) {
 
   return (
     <Card 
-      sx={{ minWidth: 275 }} 
+      sx={{
+        minWidth: 275,
+        backgroundColor: "primary.dark",
+        borderColor: `${lightBlue[700]}`,
+        boxShadow: 'rgb(0 0 0 / 10%) 0px 20px 25px, rgb(0 0 0 / 4%) 0px 10px 10px',
+        borderRadius: 2
+      }}
       onClick={handleNavigation}
+      variant="outlined"
     >
       <CardContent>
-        <Typography variant="h5" component="div">
+        <Typography variant="h5" component="div" sx={{color: `${grey[50]}`}}
+        >
           {title}
         </Typography>
-        <Typography variant="body2">
+        <Typography variant="body2" sx={{color: `${lightBlue[100]}`}}>
           {content}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Learn More</Button>
+        <Button size="small" sx={{color: `${lightBlue[50]}`}}>Learn More</Button>
       </CardActions>
     </Card>
   )
